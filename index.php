8362582259419921bcc8457efb3b42ac9654ec16
OswaldoGSDAW <147555767+OswaldoGSDAW@users.noreply.github.com>
2024-03-09 00:37:49 +0100

    Inicio de proyecto recetas

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recetario</title>
+</head>
+<body>
+    <h1>RECETARIO</h1>
+    <img src="imagenes/recetas2.png" alt="">
+</body>
+</html>
